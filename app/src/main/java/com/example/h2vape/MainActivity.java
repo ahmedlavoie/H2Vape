11529aedc6e426c0efdb8dcf0cea7292ab591a9e
ahmedlavoie <ahmed.hassan.fcis@gmail.com>
2019-10-26 17:22:03 +0200

    login and sign up implemented

--- app/src/main/java/com/example/h2vape/MainActivity.java
+++ app/src/main/java/com/example/h2vape/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.h2vape;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private EditText Email;
+    private EditText Password;
+    private Button login_btn;
+    private TextView signup_intent_txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        Email = findViewById(R.id.input_email);
+        Password = findViewById(R.id.input_password);
+        login_btn = findViewById(R.id.btn_login);
+        signup_intent_txt = findViewById(R.id.link_signup);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+        signup_intent_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignupIntent();
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private void Login() {
+        mAuth.signInWithEmailAndPassword(Email.getText().toString(), Password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                // If sign in fails, display a message to the user. If sign in succeeds
+                // the auth state listener will be notified and logic to handle the
+                // signed in user can be handled in the listener.
+                if (!task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "fe 7aga 8lt ya ba4a", Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
+            }
+        });
+
+    }
+    private void SignupIntent(){
+        Intent i = new Intent(this,SignupActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/h2vape/SignupActivity.java
@@ -0,0 +1,69 @@
+package com.example.h2vape;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText email;
+    private EditText password;
+    private Button create;
+    private TextView login;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        name = findViewById(R.id.input_name);
+        email = findViewById(R.id.input_email);
+        password = findViewById(R.id.input_password);
+        create = findViewById(R.id.btn_signup);
+        login = findViewById(R.id.link_login);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SignupActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void signup() {
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(SignupActivity.this, "Sign successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+}
